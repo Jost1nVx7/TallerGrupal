37fd90dc642e7106f05f607bd8dd4cfd12ccbb84
Jost1nVx7 <jostinjd2002@gmail.com>
2024-12-18 15:03:57 -0500

    s

--- src/main/java/espoch/edu/ec/cuentabancaria/CuentaBancaria.java
+++ src/main/java/espoch/edu/ec/cuentabancaria/CuentaBancaria.java
@@ -11,6 +11,18 @@ package espoch.edu.ec.cuentabancaria;
 public class CuentaBancaria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CuentaAhorros cuentaAhorros = new CuentaAhorros(15000, 5.0f);
+        
+         cuentaAhorros.consignar(5000);
+         cuentaAhorros.retirar(2000);
+         cuentaAhorros.extractoMensual();
+         cuentaAhorros.imprimir();
+
+         CuentaCorriente cuentaCorriente = new CuentaCorriente(5000, 3.0f);
+         
+         cuentaCorriente.consignar(2000);
+         cuentaCorriente.retirar(8000); // Sobregiro activado.
+         cuentaCorriente.extractoMensual();
+         cuentaCorriente.imprimir();
     }
 }
