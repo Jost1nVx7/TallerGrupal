6487a251103329d4f88cb9ef614e953defa7686b
Anthony Moreira <master098635@gmail.com>
2024-12-18 14:33:22 -0500

    Cambios realizados Anthony

--- src/main/java/espoch/edu/ec/cuentabancaria/Facil/CuentaCorriente.java
+++ src/main/java/espoch/edu/ec/cuentabancaria/Facil/CuentaCorriente.java
@@ -8,6 +8,45 @@ package espoch.edu.ec.cuentabancaria.Facil;
  *
  * @author Global Technology Ec
  */
-public class CuentaCorriente {
+public class CuentaCorriente extends Cuenta{
+    private float sobregiro = 0;
+
+    public CuentaCorriente(float saldo, float tasaAnual) {
+    super (saldo, tasaAnual);
+    this.sobregiro= sobregiro;
+    }
+
+    public float getSobregiro() {
+        return sobregiro;
+    }
+
+    public void setSobregiro(float sobregiro) {
+        this.sobregiro = sobregiro;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaCorriente{" + "sobregiro=" + sobregiro + '}';
+    }
+    
+    
+    @Override 
+    public void Consignar (float cantidad){
+        super.Consignar (cantidad);
+        if (sobregiro>0){
+            if (cantidad >= sobregiro){
+                cantidad -= sobregiro;
+                sobregiro=0;
+            }else{
+                sobregiro -= cantidad;
+                cantidad = 0;
+            }
+            super.Consignar (cantidad);
+        }
+        numConsignaciones++;
+    }
+
+    
+    
     
 }
