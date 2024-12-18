6cd975de65a538a830abf80a91832ecbec8cc080
Anthony Moreira <master098635@gmail.com>
2024-12-18 14:59:41 -0500

    Cambios de mi rama de Cuenta Corriente

--- src/main/java/espoch/edu/ec/cuentabancaria/Facil/CuentaCorriente.java
+++ src/main/java/espoch/edu/ec/cuentabancaria/Facil/CuentaCorriente.java
@@ -8,6 +8,49 @@ package espoch.edu.ec.cuentabancaria.Facil;
  *
  * @author Global Technology Ec
  */
-public class CuentaCorriente {
+public class CuentaCorriente extends Cuenta{
+    private float sobregiro = 0;
+
+    public CuentaCorriente(float saldo, float tasaAnual) {
+    super (saldo, tasaAnual);
+    this.sobregiro= sobregiro;
+    }
+
+    public float getSobregiro() {
+        return sobregiro;
+    }
+
+    public void setSobregiro(float sobregiro) {
+        this.sobregiro = sobregiro;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaCorriente{" + "sobregiro=" + sobregiro + '}';
+    }
+    
+    
+    @Override 
+    public void consignar (float cantidad){
+        super.consignar (cantidad);
+        if (sobregiro>0){
+            if (cantidad >= sobregiro){
+        
+            float cantidadRestante = cantidad - sobregiro;
+                sobregiro=0;
+                cantidad = cantidadRestante;
+            }else{
+                sobregiro = sobregiro - cantidad;
+                cantidad = 0;
+            }
+            if (cantidad > 0) {
+            super.consignar (cantidad);
+            }
+        }
+        numConsignaciones++;
+    }
+
+    
+    
     
 }
