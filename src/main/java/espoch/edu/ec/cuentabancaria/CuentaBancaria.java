61b6ab5aa12b007f34fcac4f6b0b99c110b4de88
Jost1nVx7 <jostinjd2002@gmail.com>
2024-12-18 15:07:13 -0500

    Pampara

--- src/main/java/espoch/edu/ec/cuentabancaria/CuentaBancaria.java
+++ src/main/java/espoch/edu/ec/cuentabancaria/CuentaBancaria.java
@@ -4,6 +4,9 @@
 
 package espoch.edu.ec.cuentabancaria;
 
+import espoch.edu.ec.cuentabancaria.Facil.CuentaAhorro;
+import espoch.edu.ec.cuentabancaria.Facil.CuentaCorriente;
+
 /**
  *
  * @author Global Technology Ec
@@ -11,14 +14,14 @@ package espoch.edu.ec.cuentabancaria;
 public class CuentaBancaria {
 
     public static void main(String[] args) {
-        CuentaAhorros cuentaAhorros = new CuentaAhorros(15000, 5.0f);
+        CuentaAhorro cuentaAhorros = new CuentaAhorro(15000, 5.0f);
         
          cuentaAhorros.consignar(5000);
          cuentaAhorros.retirar(2000);
          cuentaAhorros.extractoMensual();
          cuentaAhorros.imprimir();
 
-         CuentaCorriente cuentaCorriente = new CuentaCorriente(5000, 3.0f);
+             CuentaCorriente cuentaCorriente = new CuentaCorriente(5000, 3.0f);
          
          cuentaCorriente.consignar(2000);
          cuentaCorriente.retirar(8000); // Sobregiro activado.
--- src/main/java/espoch/edu/ec/cuentabancaria/Facil/Cuenta.java
+++ src/main/java/espoch/edu/ec/cuentabancaria/Facil/Cuenta.java
@@ -9,5 +9,49 @@ package espoch.edu.ec.cuentabancaria.Facil;
  * @author Global Technology Ec
  */
 public class Cuenta {
-    
+
+    protected float saldo;
+    protected int numConsignaciones = 0;
+    protected int numRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual = 0;
+
+    public Cuenta(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
+
+    public void consignar(float cantidad) {
+        saldo = saldo + cantidad;
+        numConsignaciones = numConsignaciones + 1;
+
+    }
+
+    public void retirar(float cantidad) {
+        if (cantidad <= saldo) {
+            saldo = saldo - cantidad;
+            numRetiros = numRetiros + 1;
+
+        } else {
+            System.out.println("!!! No se puede retirar mas que el saldo disponible!!!");
+        }
+
+    }
+
+    public void calcularInteresMensual() {
+        float interesMensual = (saldo * (tasaAnual / 100)) / 12;
+        saldo = saldo + interesMensual;
+
+    }
+    public void extractoMensual(){
+        calcularInteresMensual();
+        saldo =saldo - comisionMensual;
+    }
+    public void valores(){
+        System.out.println("saldo: "+ saldo);
+        System.out.println("Numero de consignaciones: "+ numConsignaciones);
+        System.out.println("Numero de retiros: "+ numRetiros);
+        System.out.println("Tasa anual: "+ tasaAnual);
+        System.out.println("Comision mensual: "+comisionMensual);
+    }
 }
