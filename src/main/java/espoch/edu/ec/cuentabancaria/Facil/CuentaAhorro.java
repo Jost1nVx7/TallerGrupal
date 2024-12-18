5150075197fe80d5d6dd3d028e6161234375ad92
Ribaldo  Nantip <ribaldo.nantipbarre3a@gmail.com>
2024-12-18 14:50:49 -0500

    Cuenta Ahorro

--- src/main/java/espoch/edu/ec/cuentabancaria/Facil/CuentaAhorro.java
+++ src/main/java/espoch/edu/ec/cuentabancaria/Facil/CuentaAhorro.java
@@ -8,6 +8,56 @@ package espoch.edu.ec.cuentabancaria.Facil;
  *
  * @author Global Technology Ec
  */
-public class CuentaAhorro {
+public class CuentaAhorro extends Cuenta{
+    private boolean activa;
+
+    public CuentaAhorro(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+        this.activa = saldo >= 1000;
+    }
+
+    @Override
+    public void consignar (float cantidad){
+        if (activa) {
+            super.consignar(cantidad);
+            verificarEstado();  
+        } else {
+            System.out.println("!!! La cuenta no se encuentra activa. No se puede consignar¡¡¡");
+        }
+    }
+    
+    
+    @Override
+    public void retirar (float cantidad){
+        if (activa) {
+            super.retirar(cantidad);
+            verificarEstado();     
+        } else {
+            System.out.println("!!!La cuenta no se encuentra activa. No se puede retirar¡¡¡");
+        }
+    }
+    @Override
+    private void verificarEstado(){
+        activa = saldo >= 10000;
+    }
+    
+    @Override
+    private void extractoMensual(){
+        if (numRetiros > 4) {
+            comisionMensual += (numRetiros - 4)* 1000;
+            
+        }
+        super.extractoMensual();
+        verificarEstado();
+        
+    }
+    
+    @Override
+    public void imprimir(){
+        super.imprimir();
+        System.out.println("Estado de cuenta Actuiva/Inactiva: " + activa);
+        
+    }
+
     
 }
